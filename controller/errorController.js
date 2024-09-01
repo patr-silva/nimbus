2cf773eda65ee1ecb7403364998a4816859d937a
Patrícia Silva <98959396+patr-silva@users.noreply.github.com>
2024-09-01 18:45:44 +0100

    installed vite and fixed related bugs

--- controller/errorController.js
+++
@@ -1,9 +0,0 @@
-import errorView from "../view/errorView.js";
-
-export async function init() {
-  try {
-    errorView.render();
-  } catch (error) {
-    console.error("Error in the controller " + error);
-  }
-}
